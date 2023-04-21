64026032b164858951cb9a4053febd376f021a6b
Caio <caiocsarbezerra95@gmail.com>
2023-04-20 21:11:59 -0300

    Inserção dos Nomer personalizados dos elementos do banco na tela

--- GestãoDeProjeto/Models/Projeto.cs
+++ GestãoDeProjeto/Models/Projeto.cs
@@ -11,6 +11,7 @@ namespace GestãoDeProjeto.Models
     {
         [Column("PROJNOME")]
         [StringLength(100)]
+        [Display(Name = "Nome")]
         public string nome { get; set; }
 
         [Required]
@@ -19,32 +20,37 @@ namespace GestãoDeProjeto.Models
         public int id { get; set; }
 
         [Column("PROJINICIO")]
+        [Display(Name = "Data de Início")]
         public string dataInicio { get; set; }
 
         [Column("PROJFIM")]
+        [Display(Name = "Data de Término")]
         public string dataFim { get; set; }
 
         [Column("PROJESTIMATIVA")]
+        [Display(Name = "Estimativa em meses")]
         [StringLength(100)]
         public string estimativaEmMeses { get; set; }
 
-
+        [Display(Name ="CASA")]
         [Column("PROJLOCAL")]
         public string local { get; set; }
 
         [Column("PROJVALOR")]
+        [Display(Name = "Valor Estimado do Projeto")]
         public string valorProj { get; set; }
 
         [Column("PROJSDATAAPROV")]
+        [Display(Name = "Ano de Aprovação")]
         public string anoDeAprovacao { get; set; }
 
         [Column("PROJNUMERO")]
+        [Display(Name = "Número do Projeto")]
         public string numeroDoProj { get; set; }
 
         [Column("PROJSTATUS")]
+        [Display(Name = "Situação")]
         public string status { get; set; }
-
-
 
     }
 }
--- GestãoDeProjeto/Models/Usuario.cs
+++ GestãoDeProjeto/Models/Usuario.cs
@@ -11,6 +11,7 @@ namespace GestãoDeProjeto.Models
     {
        [Required]
        [Column("USERNAME")]
+       [Display(Name = "Nome")]
         public string nome { get; set; }
 
         [Required]
@@ -18,16 +19,20 @@ namespace GestãoDeProjeto.Models
         [Column("USERID")]
         public int id { get; set; }
 
+        [Display(Name ="Data de Nascimento")]
         [Column("USERNASC")]
         public string dataNasc { get; set; }
 
+        [Display(Name = "Endereço")]
         [Column("USEREND")]
         public string endereco { get; set; }
 
         [Column("USEREMAIL")]
+        [Display(Name = "E-mail")]
         public string email { get; set; }
 
         [Column("USERCARGO")]
+        [Display(Name = "Cargo")]
         public string cargo { get; set; }
 
 
